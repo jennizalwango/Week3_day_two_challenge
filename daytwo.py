69f344976ce48f6cb4148ed9b627a34d03c3055b
mwinel <mwinel@live.com>
2018-11-14 11:21:32 -0500

    refactor day two challenge

--- daytwo.py
+++ daytwo.py
@@ -2,15 +2,26 @@ vowels = 'aieou'
 string = input('enter word')
 lista = []
 
-def get_vowel():
-    for x in string:
-        count = string.count(x)
-        if count >=1 and x in vowels:
-            lista.insert(0,x)
-            lista.append(count)
-            print(tuple( lista))
-        else:
-            print('no vowel found')
-        break
+# def count_vowel():
+#     for x in string:
+#         count = string.count(x)
+#         if count >=1 and x in vowels:
+#             lista.insert(0,x)
+#             lista.append(count)
+#             print(tuple( lista))
+#         else:
+#             print('no vowel found')
+#         break
 
-get_vowel()        
+# count_vowel()        
+
+def get_vowels():
+    count = len(string)
+    if count > 0:
+        for letter in string:
+            if letter in vowels:
+                lista.append(letter)
+        return tuple(lista)
+    else:
+        return'no vowel found'
+print(get_vowels())     
